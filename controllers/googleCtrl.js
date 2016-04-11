6efd270e7f19a6270dfe181d511100b6e3690ccf
patrickmounir <patrickmounir@gmail.com>
2016-04-11 21:55:26 +0200

    test what the angular looks like

---
+++ controllers/googleCtrl.js
@@ -0,0 +1,10 @@
+
+App.controller('googleCtrl', function($scope,Patrick,$location) {
+    //$scope.pageClass = 'page-home';
+
+    
+   $scope.goNxt=function(){
+    $location.url('/google');
+   }
+   
+});
